3f2e1397833f47467238f23ac87aec27a8ae1931
Geoffrey Berting <iamgeoffreyjr@gmail.com>
2017-12-28 11:04:53 +0800

    added content module

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 <body>
 	<?php require_once "include/nav.php"; ?>
 	<h1>Welcome to Adsurf! Great things happen!</h1>
+	<div>
+		This is the content!
+	</div>
 	<footer>
 		Contact us here!
 	</footer>
